d26fa1de394ea239b06b05612a3d549d0ca5fbbc
Filipe Marconi <filipe.fxstudio@gmail.com>
2026-07-29 12:13:55 -0300

    corrige carregamento da fonte de ícones do google material symbols

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,17 +22,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="pt-BR"
-      className={`${roboto.variable} ${roboto.className} h-full antialiased`}
-    >
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=directions_car,king_bed,bathtub,shower,straighten,open_in_full"
-        />
-      </head>
-      <body className="min-h-full flex flex-col font-sans">
+    <html lang="pt-BR" className={`${roboto.variable} h-full antialiased`}>
+      <body className={`${roboto.className} min-h-full flex flex-col font-sans`}>
         {children}
         <Toaster />
       </body>
--- components/icons/MaterialSymbol.tsx
+++ components/icons/MaterialSymbol.tsx
@@ -7,10 +7,14 @@ export type MaterialSymbolProps = HTMLAttributes<HTMLSpanElement> & {
 };
 
 /** Ícone do Google Material Symbols (fonts.google.com/icons). */
-export function MaterialSymbol({ name, className, ...props }: MaterialSymbolProps) {
+export function MaterialSymbol({ name, className, style, ...props }: MaterialSymbolProps) {
   return (
     <span
       className={cn("material-symbols-outlined inline-block shrink-0 leading-none select-none", className)}
+      style={{
+        fontFamily: '"Material Symbols Outlined"',
+        ...style,
+      }}
       aria-hidden="true"
       {...props}
     >
